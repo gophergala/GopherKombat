824326a5e269f5fd28b1454cf576a725d817fedc
Thiago Cardoso <thiagoncc@gmail.com>
2015-01-24 16:39:44 -0200

    state fields

--- common/game/types.go
+++ common/game/types.go
@@ -1,8 +1,19 @@
 package game
 
+type Gopher struct {
+	X      int  `json:"x"`
+	Y      int  `json:"y"`
+	Friend bool `json:"friend"`
+}
+
 type State struct {
-	GopherId int
-	Test     string
+	GopherId int      `json:"gid"`
+	Health   int      `json:"health"`
+	Ammo     int      `json:"ammo"`
+	Me       Gopher   `json:"me"`
+	Nearby   []Gopher `json:"nearby"`
+
+	Test string
 }
 
 type Action struct {
